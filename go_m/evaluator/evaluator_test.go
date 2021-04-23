05ab1d261ad9903ae411eb4852c25f6156e432f6
unknown <unknown@example.com>
2021-04-23 22:52:35 +0900

    go_m p131

--- go_m/evaluator/evaluator_test.go
+++ go_m/evaluator/evaluator_test.go
@@ -8,6 +8,51 @@ import (
 	"github.com/kawain/copy_interpreter/go_m/parser"
 )
 
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	// env := object.NewEnvironment()
+	// return Eval(program, env)
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
+		return false
+	}
+	return true
+}
+
 func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -36,25 +81,34 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
-func testEval(input string) object.Object {
-	l := lexer.New(input)
-	p := parser.New(l)
-	program := p.ParseProgram()
-	// env := object.NewEnvironment()
-	// return Eval(program, env)
-	return Eval(program)
-}
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+		// {"1 < 2", true},
+		// {"1 > 2", false},
+		// {"1 < 1", false},
+		// {"1 > 1", false},
+		// {"1 == 1", true},
+		// {"1 != 1", false},
+		// {"1 == 2", false},
+		// {"1 != 2", true},
+		// {"true == true", true},
+		// {"false == false", true},
+		// {"true == false", false},
+		// {"true != false", true},
+		// {"false != true", true},
+		// {"(1 < 2) == true", true},
+		// {"(1 < 2) == false", false},
+		// {"(1 > 2) == true", false},
+		// {"(1 > 2) == false", true},
+	}
 
-func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
-	result, ok := obj.(*object.Integer)
-	if !ok {
-		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
-		return false
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
 	}
-	if result.Value != expected {
-		t.Errorf("object has wrong value. got=%d, want=%d",
-			result.Value, expected)
-		return false
-	}
-	return true
 }
