0e0a81fc80cb26f4b7c10f1080f6c95404bd2ff9
unknown <unknown@example.com>
2021-05-11 21:37:13 +0900

    setrecursionlimit

--- py_m/main.py
+++ py_m/main.py
@@ -1,6 +1,9 @@
+import sys
 import getpass
 from repl_ import start
 
+# 再帰回数の上限を変更
+sys.setrecursionlimit(2000)
 
 name = getpass.getuser()
 print(f"Hello {name}! This is the Monkey programming language!")
